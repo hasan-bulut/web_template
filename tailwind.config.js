32adde1ba9a7c80771cb81350d01c91fc4b87430
Hasan Bulut <buluthasan853@gmail.com>
2024-01-11 22:55:05 +0300

    header added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js}"],
-  theme: {
+  theme: {container:{
+    center:true,
+    screens:{
+      lg:'1140px',
+      xl:'1140px',
+      '2xl':'1140px',
+    }
+  },
     extend: {},
   },
   plugins: [],
